e9797ad2edb62257f915e3d9140ffaeda327f7d7
Shona McKenzie <shona_mckenzie@hotmail.com>
2019-08-06 15:54:20 +0100

    Part B complete

--- bus.rb
+++ bus.rb
@@ -5,14 +5,35 @@ attr_reader :route_number, :destination
 def initialize(route_number, destination)
   @route_number = route_number
   @destination = destination
-  
+  @passengers = []
 end
 
 def drive
   return "Brum brum"
 end
 
+def passengers_on_bus
+  return @passengers.length
+end
 
+def pick_up(passenger1)
+  @passengers << passenger1
+  return @passengers.length
+end
+
+def drop_off(passenger2, passenger1)
+  @passengers << passenger2
+  @passengers << passenger1
+  @passengers.delete(passenger2)
+  return @passengers.length
+end
+
+def empty_bus(passenger2, passenger1)
+  @passengers << passenger2
+  @passengers << passenger1
+  @passengers = []
+  @passengers.empty?()
+end
 
 
 end
--- specs/bus_spec.rb
+++ specs/bus_spec.rb
@@ -1,16 +1,40 @@
 require('minitest/autorun')
 require('minitest/rg')
 require_relative('../bus')
+require_relative('../person')
 
 
 class BusTest < MiniTest::Test
 
   def setup
+    passengers = Person.new('Alex', 28)
     @bus = Bus.new(22, 'Ocean Terminal')
   end
 
   def test_drive_bus_sound
     assert_equal('Brum brum', @bus.drive())
   end
+
+  def test_passengers_on_bus
+    assert_equal(0, @bus.passengers_on_bus())
+  end
+
+  def test_add_person_to_passengers
+     passenger1 = Person.new("Mark", 28)
+     assert_equal(1, @bus.pick_up(passenger1))
+  end
+
+  def test_remove_passenger
+    passenger1 = Person.new("Mark", 28)
+    passenger2 = Person.new("Joe", 43)
+    assert_equal(1, @bus.drop_off(passenger2, passenger1))
+  end
+
+  def test_remove_all_passengers
+    passenger1 = Person.new("Mark", 28)
+    passenger2 = Person.new("Joe", 43)
+    assert_equal(true, @bus.empty_bus(passenger2, passenger1))
+  end
+
 
 end
